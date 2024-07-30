b1f6b48d407930110ab34ac44c5ae9070bc34c96
Anahyis <anahyisg@gmail.com>
2024-07-30 16:20:06 +0200

    begin html

---
+++ resources/views/index.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Paysans Solidaires</title>
+</head>
+<body>
+
+    <nav>
+        <ul>
+            <li><a href="">Nos produits</a></li>
+            <li><a href="">Nos producteurs</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Mon compte</a></li>
+            <li><a href="">Panier</a></li>
+        </ul>
+    </nav>
+
+    <header>
+        <h1>Titre H1</h1>
+    </header>
+
+    <main>
+        <section>
+            <h2>Qu'est-ce que l'AMAP ?</h2>
+            <img src="img/champ.jpg" alt="Image d'un champ d'agriculture">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiquat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum </p>
+            <a href="">Nous rejoindre !</a>
+        </section>
+        
+
+        <section>
+            <h2>Titre H2</h2>
+            <img src="img/producteur.png" alt="Image d'un producteur dans un champ">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiquat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum </p>
+            <a href="">Devenir producteur</a>
+        </section>
+
+
+        <section>
+            <h2>Titre H2</h2>
+            <img src="img/adherent.jpg" alt="Image d'un adhérent avec une cagette de légume">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiquat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum </p>
+            <a href="">Devenir adhérent</a>
+        </section>
+    </main>
+
+
+    <footer>
+        <a href="">Insta</a>
+        <a href="">FB</a>
+
+        <ul>
+            <li>Besoin d'aide ?</li>
+            <li><a href="">Nous contacter/a></li>
+            <li><a href="">FAQ</a></li>
+            <li><a href="">Paiement</a></li>
+        </ul>
+        <ul>
+            <li><a href="">Données personnelles</a></li>
+            <li><a href="">Conditions générales de vente</a></li>
+            <li><a href="">Mentions légales</a></li>
+            <li><a href="">Cookies</a></li>
+        </ul>
+    </footer>
+
+
+</body>
+</html>
+
