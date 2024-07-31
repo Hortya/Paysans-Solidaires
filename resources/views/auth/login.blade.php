7004bc0a42e0fe5c7b30a055ee7ea2a89bec7cee
mickael <mortharion13@gmail.com>
2024-07-31 16:54:06 +0200

    modify page login and register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,51 @@
-<x-guest-layout>
     <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>paysans-solidaires</title>
+        @vite('resources/css/app.css')
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&display=swap" rel="stylesheet">
+    </head>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+    <body class="body">
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+        <h2 class="ttl anton-regular">
+            {{ __('Connexion') }}
+        </h2>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+        <x-auth-session-status class="mb-4" :status="session('status')" />
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
+        <form class="form" method="POST" action="{{ route('login') }}">
+            @csrf
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
+            <!-- Email Address -->
+            <div class="form-login-mail">
+                <x-input-label class="txt dm-mono-regular" for="email" :value="__('Mail :')" />
+                <x-text-input id="email" class="input-itm" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            </div>
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+            <!-- Password -->
+            <div class="form-login-password">
+                <x-input-label class="txt dm-mono-regular" for="password" :value="__('Mot de passe')" />
+
+                <x-text-input id="password" class="input-itm" type="password" name="password" required autocomplete="current-password" />
+
+                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            </div>
+
+            <div class="position form-login-btn">
+
+                <x-primary-button class="btn anton-regular">
+                    {{ __('Validez') }}
+                </x-primary-button>
+            </div>
+        </form>
+    </body>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -22,25 +22,25 @@
             @csrf
 
             <!-- Firstname -->
-            <div class="position">
-                <x-input-label class="txt dm-mono-regular" for="firstname" :value="__('Prénom :')" />
-                <x-text-input id="firstname" class="input-itm-name" type="text" name="firstname" :value="old('firstname')" required autofocus autocomplete="firstname" />
+            <div class="input-txt form-name">
+                <x-input-label class="txt dm-mono-regular firstname" for="firstname" :value="__('Prénom :')" />
+                <x-text-input id="firstname" class="input-itm-name input-firstname" type="text" name="firstname" :value="old('firstname')" required autofocus autocomplete="firstname" />
                 <x-input-error :messages="$errors->get('firstname')" class="mt-2" />
 
-                <x-input-label class="txt dm-mono-regular" for="lastname" :value="__('Nom :')" />
-                <x-text-input id="lastname" class="input-itm-name" type="text" name="lastname" :value="old('lastname')" required autofocus autocomplete="lastname" />
+                <x-input-label class="txt dm-mono-regular lastname" for="lastname" :value="__('Nom :')" />
+                <x-text-input id="lastname" class="input-itm-name input-lastname" type="text" name="lastname" :value="old('lastname')" required autofocus autocomplete="lastname" />
                 <x-input-error :messages="$errors->get('lastname')" class="mt-2" />
             </div>
 
             <!-- Email Address -->
-            <div class="position">
+            <div class="form-mail">
                 <x-input-label class="txt dm-mono-regular" for="email" :value="__('Mail')" />
                 <x-text-input id="email" class="input-itm" type="email" name="email" :value="old('email')" required autocomplete="username" />
                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
             </div>
 
             <!-- Password -->
-            <div class="position">
+            <div class="form-password">
                 <x-input-label class="txt dm-mono-regular" for="password" :value="__('Mot de passe')" />
 
                 <x-text-input id="password" class="input-itm" type="password" name="password" required autocomplete="new-password" />
@@ -49,7 +49,7 @@
             </div>
 
             <!-- Confirm Password -->
-            <div class="position">
+            <div class="form-confirm-password">
                 <x-input-label class="txt dm-mono-regular" for="password_confirmation" :value="__('Confirmation mot de passe')" />
 
                 <x-text-input id="password_confirmation" class="input-itm" type="password" name="password_confirmation" required autocomplete="new-password" />
@@ -57,12 +57,12 @@
                 <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
             </div>
 
-            <div class="position">
+            <div class="form-read">
                 <input class="checkbox" type="checkbox" name="read" value="read">
                 <label class="txt dm-mono-regular" for="read">J'ai lu ...</label>
             </div>
 
-            <div class="position">
+            <div class="position form-btn">
                 <x-primary-button class="btn anton-regular">
                     {{ __('Valider') }}
                 </x-primary-button>
