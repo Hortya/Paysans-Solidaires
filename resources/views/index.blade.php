3b80eb897f24cf931e68aaeecb3b95a19bac4b37
Anahyis <anahyisg@gmail.com>
2024-07-31 16:42:37 +0200

    html-css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,8 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Paysans Solidaires</title>
+    @vite("resources/css/style.css")
 </head>
 <body>
+
+    <header class="flex">
+        <img src="img/logo_vert.svg" alt="Logo Paysans Solidaires">
+    </header>
 
     <nav>
         <ul>
@@ -17,43 +22,61 @@
         </ul>
     </nav>
 
-    <header>
-        <h1>Titre H1</h1>
-    </header>
-
-    <main>
-        <section>
-            <h2>Qu'est-ce que l'AMAP ?</h2>
+    <main class="flex column center gap">
+        <h1>L'AMAP engagée</h1>
+        <section class="flex column center">
             <img src="img/champ.jpg" alt="Image d'un champ d'agriculture">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiquat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum </p>
-            <a href="">Nous rejoindre !</a>
+                <h2>Qu'est-ce que l'AMAP ?</h2>
+                <div>
+                <p>Une AMAP (Association pour le Maintien d’une Agriculture Paysanne) est un partenariat entre des consommateurs 
+                et des producteurs locaux. </p>
+                <p> Ce système permet aux consommateurs de recevoir régulièrement des produits frais, souvent biologiques, directement des agriculteurs, tout en garantissant un revenu stable aux producteurs. 
+                Ce qui favorise une agriculture durable et locale, réduisant ainsi l’impact environnemental et soutenant l’économie locale.</p>
+                <p>
+                Les consommateurs s’engagent à acheter une part de la récolte à l’avance, ce qui permet aux agriculteurs de planifier leur production en toute sécurité. </p>
+            </div>
+            <a class="cta" href="">Nous rejoindre !</a>
         </section>
         
+        <img src="img/section1.png" alt="Image d'un champ">
 
-        <section>
-            <h2>Titre H2</h2>
-            <img src="img/producteur.png" alt="Image d'un producteur dans un champ">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiquat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum </p>
-            <a href="">Devenir producteur</a>
+        <section class="flex column center">
+            <h2>Vous êtes producteur ?</h2>
+                <img src="img/producteur.png" alt="Image d'un producteur dans un champ">
+                
+                    <p>Rejoignez notre AMAP et devenez un acteur clé dans la promotion d’une agriculture locale. En collaborant
+                    avec nous, vous bénéficierez d’un soutien direct et équitable de la part de consommateurs engagés, désireux de connaître l’origine de leurs produits 
+                    et de soutenir les producteurs locaux. </p>
+                    <p>Ensemble, nous pouvons créer un réseau solidaire qui valorise le travail des agriculteurs et garantit des produits frais et de qualité pour tous.</p>
+                </div>
+                <a class="cta" href="">Devenir Collaborateur</a>
         </section>
 
+        <img src="img/section2.png" alt="Image d'un champ">
 
-        <section>
-            <h2>Titre H2</h2>
+        <section class="flex column center">
+            <h2>Vous êtes consommateurs?</h2>
             <img src="img/adherent.jpg" alt="Image d'un adhérent avec une cagette de légume">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiquat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum </p>
-            <a href="">Devenir adhérent</a>
+            <div>
+                <p>Découvrez le plaisir de consommer des produits frais, locaux et de saison, tout en soutenant directement les agriculteurs de votre région. En devenant membre, vous participez à un réseau solidaire qui valorise le travail des producteurs et favorise une agriculture durable et respectueuse de l’environnement.</p>
+                <p>Ensemble, nous pouvons créer une communauté engagée pour une alimentation saine et responsable.</p>
+            </div>
+            <a class="cta" href="">Devenir adhérent</a>
         </section>
     </main>
 
 
-    <footer>
-        <a href="">Insta</a>
-        <a href="">FB</a>
-
+    <footer class="flex column">
+        <div class="flex column center gap30">
+            <p>Rejoignez nous sur les réseaux sociaux</p>
+            <div class="flex gap30">
+                <a href="https://www.instagram.com/" target="_blank"><img src="img/Instagram.svg" alt="Logo Instagram"></a>
+                <a href="https://www.facebook.com/" target="_blank"><img src="img/Facebook.svg" alt="Logo FB"></a>
+            </div>
+        </div>
         <ul>
-            <li>Besoin d'aide ?</li>
-            <li><a href="">Nous contacter/a></li>
+            <li class="font">Besoin d'aide ?</li>
+            <li><a href="">Nous contacter</a></li>
             <li><a href="">FAQ</a></li>
             <li><a href="">Paiement</a></li>
         </ul>
@@ -63,6 +86,7 @@
             <li><a href="">Mentions légales</a></li>
             <li><a href="">Cookies</a></li>
         </ul>
+        <div class="flex center"><img class="wid" src="img/logo_blanc.svg" alt="Logo Paysans Solidaires"></div>
     </footer>
 
 
